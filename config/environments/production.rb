d54cf0d797f9da2893e9677800a80b7d6d1d6353
Daniele Orlandi <daniele@orlandi.com>
2017-11-24 20:11:02 +0100

    Added support for servizi.acao.it frontend

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.amqp_ws_gw.allowed_request_origins = [ 'https://lino.acao.it' ]
+  config.amqp_ws_gw.allowed_request_origins = [
+    'https://lino.acao.it',
+    'https://servizi.acao.it',
+  ]
 
   config.amqp_ws_gw.routes.merge!({
     'ygg.glideradar.processed_traffic': {
