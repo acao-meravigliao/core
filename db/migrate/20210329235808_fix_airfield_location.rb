261efbcad572177361e65bac2b50a9b6f6e27088
Daniele Orlandi <daniele@orlandi.com>
2021-03-30 01:59:49 +0200

    Added migration to fix airfield location

---
+++ db/migrate/20210329235808_fix_airfield_location.rb
@@ -0,0 +1,26 @@
+class FixAirfieldLocation < ActiveRecord::Migration[6.0]
+  def change
+    fk_to_uuid('acao.airfields', 'location_id', 'core.locations')
+  end
+
+  def fk_to_uuid(from_table, column, to_table, idx_name: nil)
+    null = columns(from_table).find { |x| x.name == column }.null
+
+    if index_exists?(from_table, column)
+      remove_index from_table, column
+    end
+
+    change_column_null from_table, column, true
+    rename_column from_table, column, "#{column}_old"
+    add_column from_table, column, :uuid
+    execute "UPDATE #{from_table} SET #{column}=(SELECT id FROM #{to_table} WHERE id_old=#{column}_old)"
+    add_foreign_key from_table, to_table, column: column
+    change_column_null from_table, column, false if !null
+
+    if idx_name
+      add_index from_table, column, name: idx_name
+    else
+      add_index from_table, column
+    end
+  end
+end
