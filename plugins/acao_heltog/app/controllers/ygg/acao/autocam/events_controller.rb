e86d029e17290dbcbce9e222ac26e6b5dae6cc1f
Daniele Orlandi <daniele@orlandi.com>
2022-12-22 18:43:55 +0100

    Disabled CameraEvent for now

--- plugins/acao_heltog/app/controllers/ygg/acao/autocam/events_controller.rb
+++ plugins/acao_heltog/app/controllers/ygg/acao/autocam/events_controller.rb
@@ -16,17 +16,17 @@ class EventsController < HelTogether::Controller
 logger.info "AAAAAAAAAAAAAAAAAAAAAAAAAAAAa #{payload.inspect}"
 logger.info "AAAAAAAAAAAAAAAAAAAAAAAAAAAAb #{headers.inspect}"
 
-    Ygg::Acao::AutoCam::CameraEvent.create!(
-      event_type: headers[:type],
-      ts:, #headers[:
-      aircraft_id: payload[:traffic_id],
-      name: payload[:name]
-      flarm_id: payload[:flarm_id],
-      lat: payload[:lat],
-      lng: payload[:lng],
-      alt: payload[:alt],
-      hgt: payload[:hgt],
-    )
+#    Ygg::Acao::AutoCam::CameraEvent.create!(
+#      event_type: headers[:type],
+#      ts: Time.now, #headers[:
+#      aircraft_id: payload[:traffic_id],
+#      name: payload[:name],
+#      flarm_id: payload[:flarm_id],
+#      lat: payload[:lat],
+#      lng: payload[:lng],
+#      alt: payload[:alt],
+#      hgt: payload[:hgt],
+#    )
 
     return_from_action true
   end
