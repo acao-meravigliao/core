e2bdd2d768f7b8657eeb29254e5d361021073226
Daniele Orlandi <daniele@orlandi.com>
2024-03-25 10:43:35 +0100

    Fixed pilota() console method

--- config/initializers/console_methods.rb
+++ config/initializers/console_methods.rb
@@ -5,12 +5,12 @@ module Rails::ConsoleMethods
 
     res = Ygg::Acao::Pilot.where('lower(last_name) = ?', s.downcase)
     raise "Multiple results" if res.count > 1
-    return res if res.count == 1
+    return res.first if res.count == 1
 
     res = Ygg::Acao::Pilot.where('lower(last_name) = ? AND lower(first_name) = ?',
                            s.split(' ').last.downcase, s.split(' ').first.downcase)
     raise "Multiple results" if res.count > 1
-    return res if res.count == 1
+    return res.first if res.count == 1
 
     return nil
   end
