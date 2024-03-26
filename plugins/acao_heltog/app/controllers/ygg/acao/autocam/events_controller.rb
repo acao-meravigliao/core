a680e94433ba0585d011ce1923967517d3955e01
Daniele Orlandi <daniele@orlandi.com>
2024-03-26 17:55:30 +0100

    Added placeholder for gate controller

--- plugins/acao_heltog/app/controllers/ygg/acao/autocam/events_controller.rb
+++ plugins/acao_heltog/app/controllers/ygg/acao/autocam/events_controller.rb
@@ -20,7 +20,6 @@ class EventsController < HelTogether::Controller
 # {:id=>"6df15506-da20-41ba-9a3b-160cedb0ef3e", :name=>"D-2155", :interest=>40, :lat=>45.80895716759367, :lng=>8.77205433053044, :cog=>90, :tr=>0, :sog=>1.0269910654095578, :acc=>nil, :alt=>239.5, :hgt=>-11.5, :cr=>-0.1, :dist=>nil, :size=>26, :registration=>"D-2155", :flarm_type=>1, :flarm_id=>"DF0879", :flarm_last_rep=>nil, :tow_state=>nil, :flying_state=>"on_land", :freshness=>0.9810958496}
 # {:content_type=>"application/json", :delivery_mode=>2, :priority=>0, :timestamp=>1643979581, :type=>"CAM_TARGET_START"}
 
-
 begin
     Ygg::Acao::Autocam::CameraEvent.create!(
       event_type: headers[:type],
