cf02ab1b5cd9720711ecf7ae8757b76d3583acd3
Daniele Orlandi <daniele@orlandi.com>
2024-10-27 17:17:53 +0100

    Added frozen_string_literal: true

--- plugins/acao_maindb_models/app/models/ygg/acao/main_db/mezzo.rb
+++ plugins/acao_maindb_models/app/models/ygg/acao/main_db/mezzo.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2014-2016, Daniele Orlandi
+#
+# Author:: Daniele Orlandi <daniele@orlandi.com>
+#
+# License:: You can redistribute it and/or modify it under the terms of the LICENSE file.
+#
 
 module Ygg
 module Acao
