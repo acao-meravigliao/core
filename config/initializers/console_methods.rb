959c946d5e4bc359c61420d6092ebd626dfdb1cc
Daniele Orlandi <daniele@orlandi.com>
2025-01-03 18:42:20 +0100

    Search within last_name for pilot()

---
+++ config/initializers/console_methods.rb
@@ -0,0 +1,23 @@
+module Rails::ConsoleMethods
+  def pilota(s)
+    res = Ygg::Acao::Pilot.find_by(acao_code: s.is_a?(Numeric) ? s : (s.match(/[0-9]+/) && s.to_i))
+    return res if res
+
+    res = Ygg::Acao::Pilot.where('lower(last_name) = ?', s.downcase)
+    raise "Multiple results" if res.count > 1
+    return res.first if res.count == 1
+
+    res = Ygg::Acao::Pilot.where('\'%?%\' ILIKE last_name', s.downcase)
+    raise "Multiple results" if res.count > 1
+    return res.first if res.count == 1
+
+    res = Ygg::Acao::Pilot.where('lower(last_name) = ? AND lower(first_name) = ?',
+                           s.split(' ').last.downcase, s.split(' ').first.downcase)
+    raise "Multiple results" if res.count > 1
+    return res.first if res.count == 1
+
+    return nil
+  end
+
+  alias_method :pilot, :pilota
+end
