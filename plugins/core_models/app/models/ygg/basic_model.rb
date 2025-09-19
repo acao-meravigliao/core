0fd116c48fa12e05b59cc5513cb61f9e702e77c1
Daniele Orlandi <daniele@orlandi.com>
2025-09-19 20:30:33 +0200

    Removed attrs from BasicModel

--- plugins/core_models/app/models/ygg/basic_model.rb
+++ plugins/core_models/app/models/ygg/basic_model.rb
@@ -35,10 +35,6 @@ class BasicModel < ActiveRecord::Base
     attributes.symbolize_keys!
   end
 
-#  def attrs
-#    attrs_hash.keys
-#  end
-
   class_attribute :gs_rel_map
   self.gs_rel_map = []
 
