24792aa216684938ffb6b5f7aafd6ad0ae2f9c9d
Daniele Orlandi <daniele@orlandi.com>
2025-10-03 11:58:47 +0200

    Fixed typo

--- plugins/core_hel/app/controllers/ygg/core/person/vos_controller.rb
+++ plugins/core_hel/app/controllers/ygg/core/person/vos_controller.rb
@@ -14,7 +14,7 @@ module Core
 class Person::VosController < Ygg::Hel::VosBaseController
   def search(query:)
     if query.to_i != 0
-      res = Ygg::Acao::Member.find_by(code: query_to_i)
+      res = Ygg::Acao::Member.find_by(code: query.to_i)
       return [search_repr(res.person)] if res
     end
 
