da20f4a8de69f2a2a24d698a89b3cc139ae9fdf0
Daniele Orlandi <daniele@orlandi.com>
2025-12-05 00:28:40 +0100

    Added basic spec environment and implemented currency tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,71 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.rails_amqp.url = 'amqp://agent@linobis.acao.it'
+  config.rails_amqp.debug = 1
+
+  config.core.lc_enabled = false
+  config.core.lc_exchange = 'ygg.model.events'
+
+  config.ml.email_disabled = true
+  config.ml.email_redirect_to = 'daniele@orlandi.com'
+  config.ml.sms_disabled = true
+  config.ml.sms_redirect_to = '+393474659309'
+  config.ml.sms_skebby_debug = 2
+  config.ml.mailman_sync_disabled = true
+
+  if config.respond_to?(:acao)
+    config.acao.soci_ml_dry_run = true
+
+    config.acao.faac_dry_run = true
+    config.acao.faac_endpoint = 'https://ac-controller.acao.it/'
+    config.acao.faac_generic_user = 'acao'
+    config.acao.faac_debug = 2
+    config.acao.faac_actions = {
+      CANCELLO: '3870651b-3702-454c-ad30-42c16337ebbf',
+      SBARRA: 'e4113bd9-c6b9-401b-a157-2c37b83b5155',
+      PEDONALE: 'a93e70a7-0e62-48bb-82be-5d6c769cb6a4',
+    }
+
+    config.acao.satispay_endpoint = 'https://staging.authservices.satispay.com/'
+    config.acao.satispay_callback_url = 'http://linobis.acao.it:4201/ygg/acao/payments/satispay_callback?payment_id={uuid}'
+    config.acao.satispay_redirect_url = 'http://linobis.acao.it:4201/authen/payment/redirect-back'
+    config.acao.bar_add_maindb_transaction = false
+
+    config.acao.wp_sync_disabled = true
+    config.acao.wp_sync_dry_run = true
+    config.acao.wp_sync_debug = 0
+  end
+
+  if config.respond_to?(:rails_vos)
+    config.rails_vos.allowed_request_origins = [
+      'http://linobis.acao.it:3330',
+      'http://linobis.acao.it:3331',
+      'http://linobis.acao.it:3332',
+      'http://linobis.acao.it:4200',
+      'http://linobis.acao.it:4201',
+      'http://linobis.acao.it:4242',
+      'https://servizi-dev.acao.it',
+      'https://servizi-dev-lilc.acao.it',
+      'https://servizi-dev.lilc.acao.it',
+    ]
+
+    config.rails_vos.safe_receiver = true
+
+    config.rails_vos.routes.merge!({
+      'ygg.glideradar.processed_traffic.live.linobis': {
+        type: :topic,
+        durable: true,
+        auto_delete: false,
+        anonymous_access: true,
+      },
+      'ygg.autocam.state.linobis': {
+        type: :topic,
+        durable: true,
+        auto_delete: false,
+        anonymous_access: true,
+      },
+    })
+  end
 end
